c059bfcf3a919b832ec4040dcb45054c9e6bb2b1
Bob Ginn <bob.ginn@logicsoftware.co.uk>
2025-07-01 13:08:18 +0100

    removed unnecessary comments

--- src/Components/StarWarsHeader.vue
+++ src/Components/StarWarsHeader.vue
@@ -5,10 +5,8 @@
   </template>
   
   <script setup lang="ts">
-  // No script logic needed for this header
   </script>
   
   <style scoped>
-  /* Scoped styles if you want, but global styles already cover main look */
   </style>
   
